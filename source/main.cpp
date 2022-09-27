6cd7db66932f8a4e8715168e87bb41d43df01b6e
Neil Tan <neil@utensor.ai>
2022-09-27 16:55:23 +0800

    include path needs to be sorted; need to detele uTensor/TEST uTensor/python

--- source/main.cpp
+++ source/main.cpp
@@ -2,11 +2,50 @@
 
 MicroBit uBit;
 
-int 
-main()
-{
-    uBit.init();
+#include <stdio.h>
 
-    while(1)
-        uBit.display.scroll("HELLO WORLD!");
+#include <cmath>
+
+#include "input_image.h"  //contains the first sample taken from the MNIST test set
+#include "models/my_model/my_model.hpp"  //gernerated model file"
+#include "uTensor.h"
+
+using namespace uTensor;
+
+int argmax(const Tensor &logits) {
+  uint32_t num_elems = logits->num_elems();
+  float max_value = static_cast<float>(logits(0));
+  int max_index = 0;
+  for (int i = 1; i < num_elems; ++i) {
+    float value = static_cast<float>(logits(i));
+    if (value >= max_value) {
+      max_value = value;
+      max_index = i;
+    }
+  }
+  return max_index;
+}
+
+static My_model model;
+
+int main(void) {
+  uBit.init();
+//   printf("Simple MNIST end-to-end uTensor cli example (device)\n");
+
+  // create the input/output tensor
+  Tensor input_image = new RomTensor({1, 28, 28, 1}, flt, arr_input_image);
+  Tensor logits = new RamTensor({1, 10}, flt);
+
+  model.set_inputs({{My_model::input_0, input_image}})
+      .set_outputs({{My_model::output_0, logits}})
+      .eval();
+
+  int max_index = argmax(logits);
+  input_image.free();
+  logits.free();
+
+//   printf("pred label: %d\r\n", max_index);
+  uBit.display.print("pred label: %d\r\n", max_index);
+
+  return 0;
 }
